1f3151780f20d0b995f389e3b5c3c252201a0bd9
Le Hu <le.hu@CNle.hu>
2019-11-15 13:01:52 +0800

    init

--- src/main/java/com/hule/onion/DemoApplication.java
+++ src/main/java/com/hule/onion/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.onion;
+package com.hule.onion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/hule/onion/DemoApplicationTests.java
+++ src/test/java/com/hule/onion/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.onion;
+package com.hule.onion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
